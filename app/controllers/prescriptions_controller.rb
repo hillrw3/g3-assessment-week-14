8c5daec46a83057f74265bd06cc7cf9e170e7034
Rob Hill <robert.hill0122@gmail.com>
2014-09-08 11:25:09 -0600

    dosage must begin with an integer

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -8,19 +8,29 @@ class PrescriptionsController < ApplicationController
   def create
     starts_on = "#{params[:prescription]['starts_on(1i)']}/#{params[:prescription]['starts_on(2i)']}/#{params[:prescription]['starts_on(3i)']}"
     ends_on = "#{params[:prescription]['ends_on(1i)']}/#{params[:prescription]['ends_on(2i)']}/#{params[:prescription]['ends_on(3i)']}"
+
     @prescription = Prescription.new(patient_id: params[:prescription][:patient_id],
-                     medication_id: params[:prescription][:medication],
-                     dosage: params[:prescription][:dosage],
-                     schedule: params[:prescription][:schedule],
-                     starts_on: starts_on,
-                     ends_on: ends_on
+                                     medication_id: params[:prescription][:medication],
+                                     dosage: params[:prescription][:dosage],
+                                     schedule: params[:prescription][:schedule],
+                                     starts_on: starts_on,
+                                     ends_on: ends_on
     )
+
     @patient = Patient.find(params[:prescription][:patient_id])
-    if @prescription.save
-      flash[:notice] = "Your prescription has been created"
-      redirect_to patient_path(id: @patient.id)
+
+    if params[:prescription][:dosage][0].to_i === 1..9
+      if @prescription.save
+        flash[:notice] = "Your prescription has been created"
+        redirect_to patient_path(id: @patient.id)
+      else
+        render :new
+      end
     else
+      @prescription.errors.add(:dosage, "must begin with a number")
       render :new
     end
   end
+
+
 end
